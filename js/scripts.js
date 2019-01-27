62d3e4536feda30796f6fa0d0dc3186845a019f6
ow <volovykolya@gmail.com>
2019-01-27 11:27:46 -1000

    create js file with address book methods and objects

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+// Business Logic for AddressBook ---------
+function AddressBook(){
+  this.contacts = [],
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function (contact){
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function (){
+  this.currentId +=1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function (id){
+  for(var i=0; i<this.contacts.length; i++){
+    if(this.contacts[i]){
+      if(this.contacts[i].id === id){
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function (id){
+  for (var i=0; i<this.contacts.length; i++){
+    if(this.contacts[i]){
+      if(this.contacts[i].id === id){
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumver){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumver = phoneNumver;
+}
+
+Contact.prototype.fullName = function(){
+  return this.firstName + " " + this.lastName;
+}
+
+Contact.prototype.updateName = function(newFirstName, newLastName){
+  this.firstName = newFirstName;
+  this.lastName = newLastName;
+}
